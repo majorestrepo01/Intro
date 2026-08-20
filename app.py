69ec4ba389ecad0b031603e95d52517097eb4220
majorestrepo01 <majorestrepo01@gmail.com>
2026-08-20 07:20:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ st.write("Facilmente puedo realizar backbend y frontend.")
 image = Image.open('Pinkaura.JPG')
 st.image(image, caption='Interfaces multimodales')
 
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
