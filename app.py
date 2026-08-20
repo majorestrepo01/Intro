24fa9973087ea70f6260b359f06a6ba59e87efe9
majorestrepo01 <majorestrepo01@gmail.com>
2026-08-20 07:13:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.title("la primera app de Maria José en Streamlit")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backbend y frontend.")
+image = Image.open('Interfaces Milt.2.png')
+st.image(image, caption='Interfaces multimodales')
+
